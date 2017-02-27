088a61f57b21df3999eb13e46efa03028da03514
KAMckee <kamckee@amarillocollege.com>
2017-02-27 15:39:30 -0600

    Added ClassTime, CourseCode,CourseName, and Department properties to the Courses class.

--- COSC2330ClassProject/Courses.cs
+++ COSC2330ClassProject/Courses.cs
@@ -7,5 +7,52 @@ namespace COSC2330ClassProject
 {
     public class Courses
     {
+        public int ClassTime
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CourseName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int CourseCode
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Department
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
     }
 }
