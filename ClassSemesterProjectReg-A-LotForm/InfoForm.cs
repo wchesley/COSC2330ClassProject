e76733b67ff738fc10a654de8308b092f03dc957
Troy <monkey@BB21019>
2017-03-01 16:48:53 -0600

    Made the updateteacherform actually update the teacher profile.

---
+++ ClassSemesterProjectReg-A-LotForm/InfoForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace ClassSemesterProjectReg_A_LotForm
+{
+    public abstract class InfoForm : Form
+    {
+        public abstract void PopulateForm();
+    }
+}
