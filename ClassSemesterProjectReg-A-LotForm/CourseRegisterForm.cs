b87ad908ac3ef685da8d70fe8ba5b44d0999a0bd
KAMckee <kamckee@amarillocollege.com>
2017-03-08 15:31:45 -0600

    Deleted my unneccessary GetData mehod in the CourseRegisterForm.

--- ClassSemesterProjectReg-A-LotForm/CourseRegisterForm.cs
+++ ClassSemesterProjectReg-A-LotForm/CourseRegisterForm.cs
@@ -20,26 +20,6 @@ namespace ClassSemesterProjectReg_A_LotForm
         {
             InitializeComponent();
         }
-        private void GetData()
-        {
-            try
-            {
-                SqlConnection connection = new SqlConnection("Server=cis1.actx.edu;Database=Project1;User Id=db1;Password = db10;");
-                connection.Open();
-                SqlCommand cmd = new SqlCommand("SELECT CourseCode, CourseName, Department, CourseTime, Capacity Filled, Sections, Professor FROM dbo.CourseDatabase", connection);
-                cmd.CommandType = CommandType.Text;
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                da.Fill(ds, "ss");
-                dataGridView1.DataSource = ds.Tables["ss"];
-            }
-            catch
-            {
-                MessageBox.Show("No records found.");
-            }
-
-
-        }
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
